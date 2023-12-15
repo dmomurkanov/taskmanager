b8bc01a45306d397e492068a73d45330685ef47e
Apolon1129 <omurkanovd22@gmail.com>
2023-12-16 05:07:40 +0600

    one more time

---
+++ main/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Task(models.Model):
+    title = models.CharField('Название', max_length=250)
+    task = models.TextField('Описание')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'задача'
+        verbose_name_plural = 'задачи'
